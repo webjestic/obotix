dcaa39f5c6edb83726a102f2d995478903980d41
Michael Newlon <michael.newlon@tn.gov>
2022-10-05 20:04:04 -0400

    i16 | enhance env validate error messages

--- src/env.js
+++ src/env.js
@@ -8,17 +8,18 @@ function load() {
 }
 
 function validate() {
-    let error = []
-    if (process.env.NODE_ENV === undefined) error.push('ERROR: NODE_ENV is undefined.')
-    if (process.env.OAPI_DOMAIN === undefined) error.push('ERROR: OAPI_DOMAIN is undefined.')
-    if (process.env.OAPI_PORT === undefined) error.push('ERROR: OAPI_PORT is undefined.')
-    if (process.env.OAPI_X_API_KEY === undefined) error.push('ERROR: OAPI_X_API_KEY is undefined.')
-    if (process.env.OAPI_JWT_KEY === undefined) error.push('ERROR: OAPI_JWT_KEY is undefined.')
-    if (process.env.OAPI_MONGO === undefined) error.push('ERROR: OAPI_MONGO is undefined.')
-    if (process.env.OAPI_REDIS === undefined) error.push('ERROR: OAPI_REDIS is undefined.')
+    let errors = []
+    if (process.env.NODE_ENV === undefined) errors.push('ERROR: NODE_ENV is undefined.')
+    if (process.env.OAPI_DOMAIN === undefined) errors.push('ERROR: OAPI_DOMAIN is undefined.')
+    if (process.env.OAPI_PORT === undefined) errors.push('ERROR: OAPI_PORT is undefined.')
+    if (process.env.OAPI_X_API_KEY === undefined) errors.push('ERROR: OAPI_X_API_KEY is undefined.')
+    if (process.env.OAPI_JWT_KEY === undefined) errors.push('ERROR: OAPI_JWT_KEY is undefined.')
+    if (process.env.OAPI_MONGO === undefined) errors.push('ERROR: OAPI_MONGO is undefined.')
+    if (process.env.OAPI_REDIS === undefined) errors.push('ERROR: OAPI_REDIS is undefined.')
 
-    if (error.length > 0) {
-        console.log('ERROR: error')
+    if (errors.length > 0) {
+        for (let err in errors)
+            console.log(`NOT FOUND: ${err}`)
         return false
     }
     return true
