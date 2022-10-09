9f36b19d08502462b27a2043054a2cf53111f14e
Michael N <37873018+webjestic@users.noreply.github.com>
2022-10-09 07:30:55 -0400

    i22 | fix xpress undefined (#23)
    * i22 | fix xpress undefined
    
    * i22 | fnlib update
    
    Co-authored-by: Michael Newlon <michael.newlon@tn.gov>

--- src/index.js
+++ src/index.js
@@ -5,15 +5,20 @@ import logger from './logger.js'
 import xpress from './xpress.js'
 
 var config = undefined
-var app = xpress.app
-var express = xpress.express
+var app = undefined //xpress.app
+var express = undefined // xpress.express
 
 function setConfig(document) { config = document}
+
+async function init() { 
+    await xpress.init() 
+    app = xpress.app
+    express = xpress.express
+}
 
 function getLogger(namespace) { return logger.getLogger(namespace) }
 function setLogLevel(level) { logger.setLogLevel(level) }
 
-async function init() { await xpress.init() }
 function listen(callback) { xpress.listen(callback) }
 function getRouter() { return xpress.getRouter() }
 function addRequestMiddleware(middlware) { xpress.addRequestMiddleware(middlware) }
