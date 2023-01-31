5955c325de96f121b40c35af595917b104fc7164
Michael N <michael.newlon@tn.gov>
2023-01-31 08:27:58 -0500

    i46 | removed addMiddleware addRoute

--- src/app/http.js
+++ src/app/http.js
@@ -6,6 +6,17 @@ import express from 'express'
 import logger from './logger.js'
 import swaggerUi from 'swagger-ui-express'
 import YAML from 'yamljs'
+import 'express-async-errors'
+
+import statsmw from '../middleware/stats.js'
+import internalError from '../middleware/internalError.js'
+import notFound from '../middleware/notFound.js'
+import accesslog from '../middleware/accesslog.js'
+import dbhealth from '../middleware/dbhealth.js'
+
+import healthz from '../routes/healthz.js'
+import stats from '../routes/stats.js'
+import uuid from '../routes/uuid.js'
 
 
 class Http {
@@ -20,13 +31,30 @@ class Http {
         this.app.disable('x-powered-by')
         this.app.use(express.json())
         this.app.use(express.urlencoded({ extended: false }))
+
         this.app.use('/docs', swaggerUi.serve, swaggerUi.setup(YAML.load('config/swagger.yaml')))
+
+        this.app.use(accesslog)
+        this.app.use(dbhealth)
+        this.app.use(statsmw)
+
+        this.app.use('/', healthz(this.getRouter()))
+        this.app.use('/node', stats(this.getRouter()))
+        this.app.use('/node', uuid(this.getRouter()))
     }
 
 
     getExpress() { return express }
     getRouter() { return express.Router() }
     getApp() { return this.app }
+
+
+    listen(port, callback) {
+        this.app.use(notFound)
+        this.app.use(internalError)
+
+        this.app.listen(port, callback)
+    }
 }
 
 
