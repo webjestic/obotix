0ac5b89ee667ef11e870e90cc73a352d6daff102
Michael N <michael.newlon@tn.gov>
2023-02-24 17:29:50 -0500

    i86 | firebase using credential.cert instead of appDefault

--- src/app/firebase.js
+++ src/app/firebase.js
@@ -2,8 +2,6 @@
  * 
  */
 import logger from './logger.js'
-import shell from 'shelljs'
-import fs from 'fs'
 import admin from 'firebase-admin'
 
 class Firebase {
@@ -19,49 +17,18 @@ class Firebase {
         this.log = logger.getLogger('obx:firebase')
         this.log.info('Initializing Google Firebase.')
         
-        if (process.env.GOOGLE_APPLICATION_CREDENTIALS) {
-            try {
-                await this.decryptFirebaseFile()
-                this.log.info(`Loading ${process.env.GOOGLE_APPLICATION_CREDENTIALS}`)
-
-                this.admin = admin
-                this.admin.initializeApp({
-                    credential: this.admin.credential.applicationDefault()
-                })
-                
-                this.log.info('Firebase app successfully initialized.')
-            } catch (ex) {
-                this.log.error(ex.message, { stack: ex.stack })
-            }
-        } else {
-            this.log.error('No GOOGLE_APPLICATION_CREDENTIALS found.')
+        if (process.env.GOOGLE_FIREBASE_ADMINSDK) {
+            this.admin = admin
+            this.admin.initializeApp({
+                //credential: this.admin.credential.applicationDefault()
+                credential: this.admin.credential.cert(JSON.parse(process.env.GOOGLE_FIREBASE_ADMINSDK))
+            })
+            this.log.info('Firebase SDK successfully initialized.')
+        } else 
             throw new Error('No GOOGLE_APPLICATION_CREDENTIALS found.')
-        }
     }
-
-    async decryptFirebaseFile() {
-        try {
-            let firebaseFile = 'firebase-adminsdk.gpg'
-            if (!fs.existsSync(firebaseFile)) 
-                throw new Error(`${firebaseFile} does NOT exist.`)
-                
-            if (!fs.existsSync(process.env.GOOGLE_APPLICATION_CREDENTIALS)) {
-                // eslint-disable-next-line max-len
-                let cmd = `gpg --pinentry-mode=loopback --passphrase ${process.env.OAPI_CRYPTO_KEY} -d -o ${process.env.GOOGLE_APPLICATION_CREDENTIALS} ${firebaseFile}`
-                let result = shell.exec(cmd, {silent:true})
-                if (result.code === 0) 
-                    this.log.info(`${firebaseFile} decrypted successfully.`)
-                else
-                    this.log.error(`FAILED to decrypt ${firebaseFile}.`)
-            } else 
-                this.log.info(`${firebaseFile} has already decrypted.`)
-
-        } catch (ex) {
-            throw new Error(ex.message)
-        }
-    }
-
 }
+
 
 const firebase = new Firebase()
 export default firebase
