82e2fb028d4b277c23cc49cfa15d6b10ec0d391f
Michael N <michael.newlon@tn.gov>
2023-02-25 17:22:12 -0500

    i88 | config monitoring restored to working state

--- src/app/config.js
+++ src/app/config.js
@@ -2,10 +2,9 @@
 
 
 import logger from './logger.js'
-// import db from './db.js'
+import dbconn from '../models/config.js'
 import fs from 'fs'
 import { EventEmitter } from 'events'
-import dbconn from '../models/config.js'
 
 
 
@@ -13,7 +12,9 @@ class Config extends EventEmitter {
 
     dbconn = undefined
     defaultConfig = (JSON.parse(fs.readFileSync('config/default.json', 'utf8'))).configs
+
     log = logger.getLogger('app:config')
+
 
 
     async init() {
@@ -24,9 +25,8 @@ class Config extends EventEmitter {
             this.setConfigWatch()
             this.setupDbListeners()
             await this.loadConfig()
-            await this.checkForConfigSchemaUpdate()
         } catch(ex) {
-            this.log.error(ex)
+            this.log.error(ex.message, { stack: ex.stack })
         }
     }
 
@@ -37,11 +37,11 @@ class Config extends EventEmitter {
             try {
                 this.configChange() 
             } catch (ex) {
-                this.log.error(ex)
+                this.log.error(ex.message, { stack: ex.stack })
             }
         })
         this.dbconn.onChange.on('error', (err) => {
-            this.log.error(err)
+            this.log.error(err, { stack: err.stack } )
         })
     }
 
@@ -70,90 +70,29 @@ class Config extends EventEmitter {
             const doc = await this.dbconn.model.find({}).exec()
             this.dbconn.data = doc[0]
             if (this.dbconn.data === undefined) {
-                this.saveFirstConfig()
+                // this.saveFirstConfig()
                 this.dbconn.data = this.defaultConfig
             }
             this.log.info(`Configuration loaded from ${this.dbconn.connection.config.db}.configs.`)
             // this.log.debug('config.getConfig() returns', this.dbconn.data)
             logger.setLevel(this.dbconn.data.logger.level) // updating Logger, because it is not a subscriber
         } catch (ex) {
-            this.log.error(ex)
+            this.log.error(ex.message, { stack: ex.stack })
         }
     }
 
 
-    async saveFirstConfig() {
-        try {
-            Config.create(this.defaultConfig, (err) => {
-                if (err) return console.log(err)
-                // saved!
-            })
-        } catch(ex) {
-            this.log.error(ex)
-        }
-    }
-
-
-    async checkForConfigSchemaUpdate() {
-        this.log.trace('begin checkForConfigSchemaUpdate()')
-
-        var data = this.getConfig()
-        const id = data._id
-        this.log.debug(`configs._id= ${id}`)
-
-        var updated = false
-
-        if (data.logger === undefined) { 
-            this.log.debug('configs.logger does not exist.')
-            data.logger = this.defaultConfig.logger
-            updated = true
-        } 
-        if (data.logger.level === undefined) {
-            this.log.debug('configs.logger.level  does not exist.')
-            data.logger.level = this.defaultConfig.logger.level
-            updated = true
-        }
-        if (data.logger.logToDb === undefined) {
-            this.log.debug('configs.logger.logToDb  does not exist.')
-            data.logger.logToDb = this.defaultConfig.logger.logToDb
-            updated = true
-        }
-        if (data.logger.logToDb.enabled === undefined) {
-            this.log.debug('configs.logger.logToDb.enabled  does not exist.')
-            data.logger.logToDb.enabled === this.defaultConfig.logToDb.enabled
-            updated = true
-        }
-        if (data.logger.logToDb.level === undefined) {
-            this.log.debug('configs.logger.logToDb.level  does not exist.')
-            data.logger.logToDb.level = this.defaultConfig.logger.logToDb.level
-            updated = true
-        }
-        if (data.logger.accesslog === undefined) {
-            this.log.debug('configs.logger.accesslog  does not exist.')
-            data.logger.accesslog = this.defaultConfig.logger.accesslog
-            updated = true
-        }
-        if (data.logger.accesslog.enabled === undefined) {
-            this.log.debug('configs.logger.accesslog.enabled  does not exist.')
-            data.logger.accesslog.enabled = this.logger.accesslog.enabled
-            updated = true
-        }
-
-        if (updated === true) {
-            this.log.debug('Updating missing configs.keys.')
-            try {
-                var result = await this.dbconn.model.findByIdAndUpdate(id, data, { new: true })
-            } catch (ex) {
-                this.log.error(ex)
-            }
-            if (result === undefined) {
-                this.log.error('config.js checkForConfigSchemaUpdate() findByIdAndUpdate ERROR')
-                throw new Error('config.js checkForConfigSchemaUpdate() findByIdAndUpdate ERROR')
-            }
-        }
-
-        this.log.trace('end checkForConfigSchemaUpdate()')
-    }
+    // TODO: Flagged for deleteion; this is not a long term code comment.
+    // async saveFirstConfig() {
+    //     try {
+    //         Config.create(this.defaultConfig, (err) => {
+    //             if (err) return console.log(err)
+    //             // saved!
+    //         })
+    //     } catch(ex) {
+    //         this.log.error(ex.message, { stack: ex.stack })
+    //     }
+    // }
 
 
     getConfig() {
@@ -165,3 +104,4 @@ class Config extends EventEmitter {
 }
 const config = new Config()
 export default config
+
